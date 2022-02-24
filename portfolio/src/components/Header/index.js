e65f2029f71dd37dfd244796c441681e12146b40
Max W <mwinter@wesleyan.edu>
2022-02-24 14:39:47 -0600

    react app files

---
+++ portfolio/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "../Navigation";
+
+function Header() {
+    return(
+        <header>
+            <Nav />
+        </header>
+    );
+};
+
+export default Header;
