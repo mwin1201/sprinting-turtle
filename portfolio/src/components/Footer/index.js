176fa993a0b4ee569827cda5a215ba1d9491c887
Max W <mwinter@wesleyan.edu>
2022-02-25 14:26:41 -0600

    added icons to footer

--- portfolio/src/components/Footer/index.js
+++ portfolio/src/components/Footer/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
     return(
@@ -8,7 +10,8 @@ function Footer() {
                 &copy; Max Winter {new Date().getFullYear()}
             </h3>
             <div className="media-icons">
-                
+                <a href="https://github.com/mwin1201" target="_blank"><FontAwesomeIcon icon={faGithub} size="4x" /></a>
+                <a href="https://www.linkedin.com/in/maxwell-winter/" target="_blank"><FontAwesomeIcon icon={faLinkedin} size="4x" /></a>
             </div>
         </footer>
     );
