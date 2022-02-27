1bbdb6f5924d94e60a7dded624b6f43e67a1566d
Max W <mwinter@wesleyan.edu>
2022-02-27 17:21:11 -0600

    resume component styling

--- portfolio/src/components/Resume/index.js
+++ portfolio/src/components/Resume/index.js
@@ -2,9 +2,50 @@ import React from "react";
 
 function Resume() {
     return(
-        <div>
-            <h3>This is a resume section</h3>
-        </div>
+        <section className="resume">
+            <div className="download-link">
+                <h3>Downloadable Resume Document</h3>
+                <a href={require("../../assets/docs/2022_Resume.docx")} download>
+                    DOWNLOAD NOW
+                </a>
+            </div>
+            <div className="frontend">
+                <h3>Frontend Proficiencies:</h3>
+                <ol>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>jQuery</li>
+                    <li>Bootstrap</li>
+                    <li>Javascript</li>
+                    <li>React</li>
+                    <li>Materialize</li>
+                    <li>Handelbars</li>
+                </ol>
+            </div>
+            <div className="backend">
+                <h3>Backend Proficiencies:</h3>
+                <ol>
+                    <li>MySQL</li>
+                    <li>MongoDB</li>
+                    <li>Heroku</li>
+                    <li>Express.js</li>
+                    <li>Node.js</li>
+                    <li>Sequelize</li>
+                    <li>Mongoose</li>
+                </ol>
+            </div>
+            <div className="other">
+                <h3>Other:</h3>
+                <ul>
+                    <li>PWAs</li>
+                    <li>bcrypt</li>
+                    <li>Font Awesome</li>
+                    <li>Git</li>
+                    <li>Jest</li>
+                    <li>Inquirer</li>
+                </ul>
+            </div>
+        </section>
     );
 };
 
