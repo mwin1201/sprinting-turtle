74653acaebcffc8556675a2e571034f84d734eb9
Max W <mwinter@wesleyan.edu>
2022-03-02 14:06:24 -0600

    added animation to home page button

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -1,17 +1,24 @@
 import React from "react";
 import max_pic from "../../assets/img/max_picture.jpg";
+import { useSpring, animated } from "react-spring";
 
 function About() {
+
+    const styles = useSpring({ 
+        from: { opacity: 0, marginLeft: -100, marginRight: 100 },
+        to: { opacity: 1, marginLeft: 0, marginRight: 0 }
+    });
+
     return(
         <>
         <div className="hero-img">
             <div className="hero-text">
                 <h1>Hi, I'm Max. A full stack web developer.</h1>
-                <button>About me</button>
+                <animated.button style={styles}><a href="#about">About me</a></animated.button>
             </div>
         </div>
         <div className="title">
-            <h2>About Me</h2>
+            <h2 id="about">About Me</h2>
         </div>
         <div className="bio-section">
             <div className="bio">
